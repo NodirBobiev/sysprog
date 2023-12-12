e3c3bac0a418f0b1ad3cbf4dd0db6c2eb4fd06d2
bobievnodir <bobievnodir@yandex-team.ru>
2023-12-12 21:07:23 +0300

    add bench_param to pthread_create_join.c

--- bonus/pthread_create_join.c
+++ bonus/pthread_create_join.c
@@ -11,7 +11,7 @@ void* thread_worker (void *args)
     return NULL;
 }
 
-void main()
+double benchmark()
 {
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC, &start);
@@ -25,6 +25,27 @@ void main()
     struct timespec end;
     clock_gettime(CLOCK_MONOTONIC, &end);
 
-    long long totalTime = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
-    printf("ITERATIONS: %d \t %.2f ns per create/join pair\n", ITERATIONS, (double)totalTime / ITERATIONS);
+    double result = ((end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec))/(double)ITERATIONS;
+    printf("ITERATIONS: %d \t %.2f ns per create/join pair\n", ITERATIONS, result);
+
+    return result;
+}
+
+
+int compare(const void* a, const void* b) {
+    return (*(double*)a - *(double*)b) > 0;
+}
+void display(double arr[], int len) {
+    qsort(arr, len, sizeof(double), compare);
+    printf("min: %.2f \t median: %.2f \t max: %.2f\n\n", arr[0], arr[len / 2], arr[len-1]);
+}
+
+int main(){
+        
+    int M = 5;
+    double arr[M];
+    for (int j = 0; j < M; j ++ )
+        arr[j] = benchmark();
+    display(arr, 5);
+
 }
