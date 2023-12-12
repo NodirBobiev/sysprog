4aed8374c33b626776712fc6753bf43b19448809
bobievnodir <bobievnodir@yandex-team.ru>
2023-12-12 20:55:48 +0300

    add bench param to clock_bench

--- bonus/clock_bench.c
+++ bonus/clock_bench.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
 #include<bits/types/clockid_t.h>
 
 #define ITERATIONS 50000000
 
-void benchmark(const char* label, clockid_t clockType) {
+double benchmark(const char* label, clockid_t clockType) {
     struct timespec start, end;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
@@ -13,17 +14,42 @@ void benchmark(const char* label, clockid_t clockType) {
         clock_gettime(clockType, &currentTime);
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
-    long long totalTime = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+    double result = ((end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec))/(double)ITERATIONS;
 
-    printf("%20s: \t %.2f ns per call\n", label, (double)totalTime / ITERATIONS);
+    printf("%20s: \t %.2f ns per call\n", label, result);
+    return result;
 }
 
+int compare(const void* a, const void* b) {
+    return (*(double*)a - *(double*)b) > 0;
+}
+void display(double arr[], int len) {
+    qsort(arr, len, sizeof(double), compare);
+    printf("min: %.2f \t median: %.2f \t max: %.2f\n\n", arr[0], arr[len / 2], arr[len-1]);
+}
+
+struct bench_param{
+    const char* label;
+    clockid_t clockType;
+};
+
+struct bench_param parameters[]={
+    {"CLOCK_REALTIME", CLOCK_REALTIME},
+    {"CLOCK_MONOTONIC", CLOCK_MONOTONIC},
+    {"CLOCK_MONOTONIC_RAW", CLOCK_MONOTONIC_RAW},
+};
+
 int main() {
-    benchmark("CLOCK_REALTIME", CLOCK_REALTIME);
+    int N = sizeof(parameters) / sizeof(parameters[0]);
 
-    benchmark("CLOCK_MONOTONIC", CLOCK_MONOTONIC);
 
-    benchmark("CLOCK_MONOTONIC_RAW", CLOCK_MONOTONIC_RAW);
+    int M = 5;
+    for (int i = 0; i < N; i ++ ){
+        double arr[M];
+        for (int j = 0; j < M; j ++ )
+            arr[j] = benchmark(parameters[i].label, parameters[i].clockType);
+        display(arr, 5);
+    }
 
     return 0;
 }
