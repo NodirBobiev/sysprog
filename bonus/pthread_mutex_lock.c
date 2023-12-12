e022e51870556c0cd8f839337dc38cb8fc86e58a
bobievnodir <bobievnodir@yandex-team.ru>
2023-12-12 21:11:41 +0300

    add min, medium, max to pthread_mutex_lock

--- bonus/pthread_mutex_lock.c
+++ bonus/pthread_mutex_lock.c
@@ -18,7 +18,7 @@ void* thread_worker (void *args)
     return NULL;
 }
 
-void benchmark(int threads_count)
+double benchmark(int threads_count)
 {
     pthread_t *threads = malloc(sizeof(pthread_t)*threads_count);
     mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
@@ -40,16 +40,30 @@ void benchmark(int threads_count)
     struct timespec end;
     clock_gettime(CLOCK_MONOTONIC, &end);
 
-    long long totalTime = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
-    printf("threads_count: %d \t ITERATIONS: %d \t %.2f ns per lock/unlock pair\n", threads_count, ITERATIONS, (double)totalTime / ITERATIONS);
+    double result = ((end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec))/(double)ITERATIONS;
+    printf("threads_count: %d \t ITERATIONS: %d \t %.2f ns per lock/unlock pair\n", threads_count, ITERATIONS, result);
 
     pthread_mutex_destroy(mutex);
     free(threads);
     free(mutex);
+    return result;
+}
+
+int compare(const void* a, const void* b) {
+    return (*(double*)a - *(double*)b) > 0;
+}
+void display(double arr[], int len) {
+    qsort(arr, len, sizeof(double), compare);
+    printf("min: %.2f \t median: %.2f \t max: %.2f\n\n", arr[0], arr[len / 2], arr[len-1]);
 }
 
 int main(){
-    for(int i = 1; i <= 10; i ++ ){
-        benchmark(i);
+    for (int i = 1; i <= 10; i ++ ){
+        int M = 5;
+        double arr[M];
+        for (int j = 0; j < M; j ++ )
+            arr[j] = benchmark(i);
+        display(arr, 5);
     }
+
 }
